e79777c7e894e7c97d17b0c103eba6562be9b685
Rohit Karthik <rohitismaster@gmail.com>
2025-07-12 16:17:12 -0400

    added spritesheet

---
+++ src/include/graphics/spritesheet.h
@@ -0,0 +1,11 @@
+#ifndef SPRITESHEET_H
+#define SPRITESHEET_H
+
+#include <stdint.h>
+
+#define SPRITESHEET_SPRITE_COUNT 36
+#define SPRITESHEET_SPRITE_SIZE 8  // 8 bytes per sprite (8x8 pixels)
+
+extern const uint8_t spritesheet[SPRITESHEET_SPRITE_COUNT * SPRITESHEET_SPRITE_SIZE];
+
+#endif
